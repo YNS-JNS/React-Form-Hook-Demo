6ef720bd0b9315c87dddcdb477835c9347f8cbcb
yns-jns <younessaitmbarek97@gmail.com>
2024-02-24 19:57:02 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './'
+// Import react-hook-form package
+import { useForm } from 'react-hook-form';
 
-function App() {
+const App = () => {
+
+  const {
+    register, // 
+    handleSubmit, // 
+    // watch, // 
+    formState: { errors }, // 
+  } = useForm();
+
+  const handleSaveDate = (payload) => {
+
+    console.log(payload);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="container">
+      <h1>
+        React ⚛️
+      </h1>
+      <h2>
+        React-Form-Hook ⚡ + Pico Css 🌀
+      </h2>
+      {/* Form:  */}
+      <form onSubmit={handleSubmit(handleSaveDate)}>
+        <section className='form_section'>
+          <label>
+            Name
+            <input
+              type='text'
+              name="name"
+              placeholder="First name"
+              autoComplete="given-name"
+              {...register("name", { required: true })}
+            />
+          </label>
+          {errors.name && <span>name is required!</span>}
+          <label>
+            Email
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              autoComplete="email"
+              {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i })}
+            />
+          </label>
+          {errors.email && errors.email.type === 'required' && <span>email is required!</span>}
+          {errors.email && errors.email.type === 'pattern' && <span>email is invalid!</span>}
+        </section>
+        <center>
+          <button type='submit' >Submit</button>
+        </center>
+      </form>
+    </main>
+  )
 }
 
 export default App;
